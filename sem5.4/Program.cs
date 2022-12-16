7e123f6eee6510406f4b0958e629c56d16ab7575
сергей <dman.2055@yandex.ru>
2022-12-16 19:28:08 +0300

    Add dz5

---
+++ sem5.4/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 35. Задайте одномерный массив из 123 случайных чисел.
+// Найдите поличество элементов массива, значения которых лежат в 
+// отрезке [10, 99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+// 123
+// [5, 12, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int [] CreatArrayRndInt (int size, int min, int max)
+{
+    int [] array = new int [size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length -1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("] ");
+}
+
+
+int NumberOfElements(int [] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99 ) result++;
+    }
+    return result;
+}
+
+int [] arr = CreatArrayRndInt(123, 0, 1000);
+PrintArray(arr);
+
+int res = NumberOfElements(arr);
+Console.Write($"--> {res}");
